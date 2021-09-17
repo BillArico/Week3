ea8b3daef9c764f1a1c0f988ed7ef692e6dc2d26
BillArico <89521166+BillArico@users.noreply.github.com>
2021-09-17 00:12:21 -0400

    Added toast, snackbar

--- app/src/main/java/com/example/week3/MainActivity.java
+++ app/src/main/java/com/example/week3/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    int i=0;
+    TextView mTextView;
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent recInt= getIntent();
+
+        mTextView= (TextView)findViewById(R.id.tv1);
+        mTextView.setText(""+recInt.getIntExtra("THESUM",0));
+        mTextView.setOnClickListener(view -> {
+            ((TextView)view).setTextSize(45);
+        });
+
+        mButton= (Button)findViewById(R.id.btn3);
+        mButton.setOnClickListener(this::toShowToastMSG);
+
+    }
+
+    private void toShowToastMSG(View view) {
+        Toast.makeText(MainActivity.this, "Bill Arico",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void toShowSBMSG(View view) {
+        Snackbar sb= Snackbar.make(view,"Arico, Bill",
+                Snackbar.LENGTH_LONG);
+        sb.show();
+    }
+
+
+    class MyLsntr implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+
+
+
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
     }
 }
---
+++ app/src/main/java/com/example/week3/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.week3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    EditText mEditText1, mEditText2;
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mEditText1 = (EditText) findViewById(R.id.et1);
+        mEditText2 = (EditText) findViewById(R.id.et2);
+
+        mTextView= (TextView) findViewById(R.id.tv1);
+    }
+
+    public void calculateSum(View view) {
+        int sm= Integer.parseInt(mEditText1.getText().toString()) +
+                Integer.parseInt(mEditText2.getText().toString());
+
+        mTextView.setText(""+sm);
+
+        Intent n= new Intent(this, MainActivity.class);
+        n.putExtra("THESUM",sm);
+        startActivity(n);
+
+
+    }
+}
